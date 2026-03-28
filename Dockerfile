2ea3838587960648725a6d1fd3e48a3bde199056
Santi <yepes.s07@gmail.com>
2026-03-28 04:46:36 -0500

    Config Firebase ruta JSON

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM maven:3.9.6-eclipse-temurin-21-jammy AS build
+FROM eclipse-temurin:21-jdk-jammy AS build
 WORKDIR /app
 
 COPY pom.xml .
--- src/main/java/com/example/sucursal/config/FirebaseConfig.java
+++ src/main/java/com/example/sucursal/config/FirebaseConfig.java
@@ -5,13 +5,9 @@ import com.google.cloud.firestore.Firestore;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
-import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.FileInputStream;
 
 @Configuration
@@ -19,13 +15,10 @@ public class FirebaseConfig {
 
     @Bean
     public Firestore getFirestore() throws IOException {
-        // Verificamos si ya existe una instancia para evitar el error "already exists"
         if (FirebaseApp.getApps().isEmpty()) {
             
-            // 1. Intentamos leer la ruta desde una variable de entorno (Ideal para Render)
-            // Si no existe, usamos el nombre por defecto "./service-account.json"
             String path = System.getenv("FIREBASE_CONFIG_PATH");
-            if (path == null) path = "./service-account.json";
+            if (path == null) path = "src/main/resources/service-account.json";
 
             try (FileInputStream serviceAccount = new FileInputStream(path)) {
                 FirebaseOptions options = FirebaseOptions.builder()
@@ -34,7 +27,7 @@ public class FirebaseConfig {
 
                 FirebaseApp.initializeApp(options);
             } catch (IOException e) {
-                System.err.println("CRÍTICO: No se pudo encontrar el archivo de credenciales en: " + path);
+                System.err.println("No se pudo encontrar el archivo de credenciales en: " + path);
                 throw e;
             }
         }
